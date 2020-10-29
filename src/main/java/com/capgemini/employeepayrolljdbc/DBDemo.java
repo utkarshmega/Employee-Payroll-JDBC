b8dbce4d6be239e51187a873f14bb8661046c4d4
utkarshmega <utkarsheduc@gmail.com>
2020-10-29 13:14:02 +0530

    Welcome to the jdbc connectivity in employee payroll

---
+++ src/main/java/com/capgemini/employeepayrolljdbc/DBDemo.java
@@ -0,0 +1,12 @@
+package com.capgemini.employeepayrolljdbc;
+
+public class DBDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the jdbc connectivity");
+
+
+	}
+
+}
