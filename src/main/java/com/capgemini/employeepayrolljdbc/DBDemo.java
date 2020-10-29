5f262b02249f529554c5c3807d07c66fcca30ed6
utkarshmega <utkarsheduc@gmail.com>
2020-10-30 00:08:44 +0530

    Connection made to the jdbc

--- src/main/java/com/capgemini/employeepayrolljdbc/DBDemo.java
+++ src/main/java/com/capgemini/employeepayrolljdbc/DBDemo.java
@@ -1,12 +1,39 @@
 package com.capgemini.employeepayrolljdbc;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
 public class DBDemo {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Welcome to the jdbc connectivity");
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+		String username = "root";
+		String password = "sqldatabase@1252";
+		Connection connection;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded");
+		} catch(ClassNotFoundException e) {
+			throw new IllegalStateException("Cannot find the driver in the classpath! "+e);
+		}
+		listDrivers();
+		try {
+			System.out.println("Connecting to the database: "+jdbcURL);
+			connection = DriverManager.getConnection(jdbcURL, username, password);
+			System.out.println("Connection Sucessful!!" +connection);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 
-
+	}
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while(driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println(" "+ driverClass.getClass().getName());
+		}
 	}
 
 }
